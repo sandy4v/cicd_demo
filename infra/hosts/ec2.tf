13a38611e25a2727d4f3ca44a24a8ae940cbb4ad
Sandeep Patharkar <sandy4v@gmail.com>
2019-10-10 10:13:27 -0400

    Update ec2.tf

--- infra/hosts/ec2.tf
+++ infra/hosts/ec2.tf
@@ -48,6 +48,12 @@ resource "aws_security_group" "sg_pub" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port = "${var.ssh_port}"
